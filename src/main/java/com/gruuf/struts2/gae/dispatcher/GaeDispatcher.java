aa41182b8e32737d6c481ea0b0f142b66e9b08b6
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-12-17 10:45:55 +0100

    Turns comment into log statement

--- src/main/java/com/gruuf/struts2/gae/dispatcher/GaeDispatcher.java
+++ src/main/java/com/gruuf/struts2/gae/dispatcher/GaeDispatcher.java
@@ -1,5 +1,7 @@
 package com.gruuf.struts2.gae.dispatcher;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts2.dispatcher.Dispatcher;
 
 import javax.servlet.ServletContext;
@@ -7,13 +9,15 @@ import java.util.Map;
 
 public class GaeDispatcher extends Dispatcher {
 
+    private static final Logger LOG = LogManager.getLogger(GaeDispatcher.class);
+
     GaeDispatcher(ServletContext servletContext, Map<String, String> params) {
         super(servletContext, params);
     }
 
     @Override
     protected String getSaveDir() {
-        // Google AppEngine doesn't allow to define a custom save dir
+        LOG.debug("Google AppEngine doesn't allow to define a custom save dir");
         return null;
     }
 }
